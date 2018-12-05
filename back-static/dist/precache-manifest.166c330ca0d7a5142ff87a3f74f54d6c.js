2c187b3836a535aa2481e344670ffd6773c669d6
linbenjian <linbenjian@tuzhanai.com>
2018-12-05 16:10:09 +0800

    back build

---
+++ back-static/dist/precache-manifest.166c330ca0d7a5142ff87a3f74f54d6c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "a782baa8633b1359f9686ffad17e0d76",
+    "url": "/fonts/fontello.a782baa8.woff"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "068ca2b316db98037bebdd1e4f1b9459",
+    "url": "/fonts/fontello.068ca2b3.ttf"
+  },
+  {
+    "revision": "58d194073a4f9803ac31",
+    "url": "/css/chunk-vendors.bcdd606f.css"
+  },
+  {
+    "revision": "2fad952a20fbbcfd1bf2ebb210dccf7a",
+    "url": "/fonts/element-icons.2fad952a.woff"
+  },
+  {
+    "revision": "6f0a76321d30f3c8120915e57f7bd77e",
+    "url": "/fonts/element-icons.6f0a7632.ttf"
+  },
+  {
+    "revision": "8d4a4e6f7431d0d7fa92b1df20f38161",
+    "url": "/fonts/fontello.8d4a4e6f.woff2"
+  },
+  {
+    "revision": "3b8bb723474aa57eb63b",
+    "url": "/js/app.2e0bbe77.js"
+  },
+  {
+    "revision": "58d194073a4f9803ac31",
+    "url": "/js/chunk-vendors.746b9e7c.js"
+  },
+  {
+    "revision": "e73a0647198cfe970de0f003be95cc51",
+    "url": "/fonts/fontello.e73a0647.eot"
+  },
+  {
+    "revision": "9354499c2824248511adf85fdf8e4c37",
+    "url": "/img/fontello.9354499c.svg"
+  },
+  {
+    "revision": "6c6339f9f81de293fdb573f306017547",
+    "url": "/img/icon-type.6c6339f9.svg"
+  },
+  {
+    "revision": "46aa2a78a1792c729b625c957710cc6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b8bb723474aa57eb63b",
+    "url": "/css/app.6051e637.css"
+  }
+];
