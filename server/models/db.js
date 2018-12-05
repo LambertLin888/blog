ece4617612814d67431df40f891d3e81b7bb5f98
linbenjian <linbenjian@tuzhanai.com>
2018-12-05 18:38:40 +0800

    恢复db 链接方式

--- server/models/db.js
+++ server/models/db.js
@@ -11,7 +11,8 @@ const dbUrl =
   process.env.NODE_ENV == "production"
     ? "mongodb://127.0.0.1/blog-prod-db"
     : "mongodb://127.0.0.1/lambert-db";
-const db = mongoose.createConnection(dbUrl, { useNewUrlParser: true });
+// const db = mongoose.createConnection(dbUrl, { useNewUrlParser: true });
+const db = mongoose.createConnection(dbUrl);
 
 db.once("open", callback => {
   console.log("数据库连接成功");
