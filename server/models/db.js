fa2f2436c676534adb2ef70c4b49a93470fd8167
linbenjian <linbenjian@tuzhanai.com>
2018-12-06 10:18:35 +0800

    server contet add timeout

--- server/models/db.js
+++ server/models/db.js
@@ -11,7 +11,13 @@ const dbUrl =
   process.env.NODE_ENV == "production"
     ? "mongodb://127.0.0.1/blog-prod-db"
     : "mongodb://127.0.0.1/lambert-db";
-const db = mongoose.createConnection(dbUrl, { useNewUrlParser: true });
+const option = {
+  socketTimeoutMS: 30000,
+  keepAlive: true,
+  reconnectTries: 30000,
+  useNewUrlParser: true
+};
+const db = mongoose.createConnection(dbUrl, option);
 
 db.once("open", callback => {
   console.log("数据库连接成功");
