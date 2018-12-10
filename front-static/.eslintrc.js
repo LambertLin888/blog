3405bc2cde281d39f20ba61fb053f3622b2e58fb
linbenjian <linbenjian@tuzhanai.com>
2018-12-10 19:15:10 +0800

    tslint vue去除换行

--- front-static/.eslintrc.js
+++ front-static/.eslintrc.js
@@ -7,11 +7,12 @@ module.exports = {
   parserOptions: {
     parser: 'babel-eslint'
   },
-  extends: ['plugin:vue/recommended', 'plugin:prettier/recommended'],
+  extends: ['plugin:prettier/recommended', 'plugin:vue/recommended'],
   // required to lint *.vue files
   plugins: ['vue', 'prettier'],
   // add your custom rules here
   rules: {
+    'vue/max-attributes-per-line': 'off',
     'prettier/prettier': 'error',
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
