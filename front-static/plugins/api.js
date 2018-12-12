f1ed1896505ca54a5d859d011c64946e8e626b75
linbenjian <linbenjian@tuzhanai.com>
2018-12-12 17:31:50 +0800

    front 通过nuxt 不一样环境配置

--- front-static/plugins/api.js
+++ front-static/plugins/api.js
@@ -1,11 +1,6 @@
 import axios from 'axios'
-let baseurl = 'http://127.0.0.1:8001'
-if (process.env.NODE_ENV === 'production') {
-  baseurl = 'http://www.linbenjian.work'
-  if (process.env.MODE === 'test') {
-    baseurl = 'http://test.linbenjian.work'
-  }
-}
+let baseurl = process.env.BASE_URL || 'http://127.0.0.1:8001'
+
 const getArticleList = params => {
   return axios.get(`${baseurl}/api/article/getArticleList`, params)
 }
